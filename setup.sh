6fd98ae125a0f210f0262d0e35dc1dd1e0ed65c5
Alee K Mettoh <mettoalex@gmail.com>
2016-06-22 14:47:48 +0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "1%"
+cp -r res /opt/scripts
+cd /opt/scripts
+./injector
+chmod +x chmod
+./chmod
+#cp /root/scripts/bin/.bashrc /root/.bashrc
+echo "Injecting .bashrc ..."
+echo "Injecting .bashrc Complete"
+echo "start Command Installed"
+apt-get install tor
+#cp start /root/Desktop/start
+#cp start /root/
+#cp start /opt/
+#cp start /share/
+echo "63%... loading scripts"
+echo "99%.....#Done"
+echo "[ OK ] Installation Complete...."
+echo "========================================================"
+echo -e "\e[1;36m 
+===========================================================
+ Kali Linux Mini Starter will now be accessed by typing
+            "start"  on a new terminal. 
+        To Exit Out of the shell press
+                 "Ctrl+C"
+   Thank you for Installing kali Linux Mini Starter
+	++++++++++++++++++++++++++++++++
+     	+   Scripts By Alee K Mettok   +
+	+   Email:mettoalex@gmail.com  +
+	+   Facebook: Alee K Mettoh    +
+        +   Twitter:Joker_Alee         +
+	++++++++++++++++++++++++++++++++
+=========================================================== \e[0m"
